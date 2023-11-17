d21a288686f204a5a410fb528280dcf454b633ba
dravasilis <dravasilis@gmail.com>
2023-11-17 17:46:23 +0200

    buttons

--- src/app/button/button.component.ts
+++ src/app/button/button.component.ts
@@ -1,11 +1,20 @@
-import { Component, Input } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
 
 @Component({
   selector: 'app-button',
   templateUrl: './button.component.html',
   styleUrls: ['./button.component.css']
 })
-export class ButtonComponent {
- @Input()
- description = '';
+export class ButtonComponent {    
+  btnDefault = 'btn-default';
+  @Input()isClicked = false;
+  @Input() isSelected?:boolean;
+  @Output() isSelectedChange= new EventEmitter<void>()
+  btnChange = 'btn-change'
+  btnClicked = this.btnDefault; 
+
+  clicked(){
+    this.isSelectedChange.emit()
+  }
+  if(){}
 }
--- src/app/the-hotel/the-hotel.component.ts
+++ src/app/the-hotel/the-hotel.component.ts
@@ -5,6 +5,21 @@ import { Component } from '@angular/core';
   templateUrl: './the-hotel.component.html',
   styleUrls: ['./the-hotel.component.css']
 })
-export class TheHotelComponent {
-
+export class TheHotelComponent { 
+  isSelected = 'Halkidiki';
+  buttons=['Halkidiki','Hotel Melissanthi'];
+  menu = [{
+    image: 'https://firebasestorage.googleapis.com/v0/b/smart-catalog-a2538.appspot.com/o/products%2F__business_legacy_39%2F1582206065805_product.jpg?alt=media&token=f37367b5-50c9-4087-9127-b5ea7384c5ea',
+    title: 'Map'
+  },
+  {
+    image: 'https://firebasestorage.googleapis.com/v0/b/smart-catalog-a2538.appspot.com/o/products%2F__business_legacy_39%2F1582205702041_product.jpg?alt=media&token=e324a934-2c43-4ee3-a5ff-3bb41e7f44e6',
+    title: 'Beach Bars'
+  },
+  {
+    image: 'https://firebasestorage.googleapis.com/v0/b/smart-catalog-a2538.appspot.com/o/products%2F__business_legacy_39%2F1582206110037_product.jpg?alt=media&token=907b3bf0-f30f-4451-b107-856cb2fb867c',
+    title: 'Events-Concerts-Theatre'
+  }];
 }
+
+
